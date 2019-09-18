93463d8e22b9e155f5568d2900d51a4949705e39
famesensor <famfew25@gmail.com>
2019-09-19 00:34:20 +0700

    new

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ const posts = require('./routers/api/posts');
 
 app.use('/api/posts',posts);
 
+// Handle production
+if(process.env.NODE_ENV === 'production') {
+    // Static foloder
+    app.use(express.static(__dirname + '/public'));
+
+    // Handle SPA
+    app.get(/.*/, (req, rs) => res.sendFile(__dirname + 'public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log('Server stared on port '+port));
